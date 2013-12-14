39853597f07dde3238673982d011e44a4b5f3411
europrapor <europrapor@gmail.com>
2013-12-14 20:44:57 +0100

    go

---
+++ valform.php
@@ -0,0 +1,28 @@
+<script>
+$(document).bind("mobileinit", function(){
+        $.mobile.ajaxenabled = false;
+    });
+	</script>
+	
+<?php
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+{
+  $ip=$_SERVER['HTTP_CLIENT_IP'];
+}
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+{
+  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+else
+{
+  $ip=$_SERVER['REMOTE_ADDR'];
+}
+
+$datetime = new DateTime();
+
+
+
+$line= json_encode(array('ip' => $ip)) . ' '. date('Y-m-d h:i:s') . ' ' . $datetime->format('U') . ' ' . $_POST['resolution']  . ' ' .$_POST['slider'] . ' ' .$_POST['slider1'] . ' ' .$_POST['slider2'] . ' ' .$_POST['slider3'] . ' ' .$_POST['slider4'];
+echo $line;
+?>
