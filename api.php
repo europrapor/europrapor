1e8f159c4e1a0467dcfd338f63b38bc5e9da421c
Oleksii Matiiasevych <lastperson@gmail.com>
2013-12-17 21:15:25 +0200

    Implement prototype GET api.

--- api.php
+++ api.php
@@ -1,53 +1,72 @@
 <?php
 
+$maps = ['joy', 'fear', 'determination', 'anger'];
+$response = '';
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (preg_match('/^\/map\/(\w+)$/', $_SERVER['SCRIPT_NAME'], $matches) && in_array($matches[1], $maps)) {
+        include('mysql_pdo_conn.php');
+        $sql = 'SELECT id, unixtime, rep1 AS coords, rep2 AS security, rep3 AS joy, rep4 AS fear, rep5 AS determination, divers AS anger FROM alpha_rows';
+        $rows = $conn->query($sql);
+        if (!$rows) {
+            echo 'An SQL error occured.\n';
+            exit(1);
+        }
+
+        $response = array();
+        while ($row = $rows -> fetch(PDO::FETCH_ASSOC)) {
+            $heat = [
+                'id' => intval($row['id']),
+                'heat' => intval($row[$matches[1]])
+            ];
+
+            array_push($response, $heat);
+        }
+    }
+
     switch ($_SERVER['SCRIPT_NAME']) {
         case '/maps':
-            header('Content-type: application/json');
-            header("HTTP/1.0 200 OK");
-            echo '["joy", "fear", "determination", "anger"]';
+            $response = $maps;
             break;
         case '/map':
-            header('Content-type: application/json');
-            header("HTTP/1.0 200 OK");
-            echo '[
-                      {
-                          "id": 1,
-                          "position": {
-                              "lt": 35.45645,
-                              "lg": 67.987986
-                          }
-                      },
-                      {
-                          "id": 2,
-                          "position": {
-                              "lt": 35.45645,
-                              "lg": 67.987986
-                          }
-                      }
-                  ]';
+            include('mysql_pdo_conn.php');
+            $sql = 'SELECT id, unixtime, rep1 AS coords, rep2 AS security, rep3 AS joy, rep4 AS fear, rep5 AS determination, divers AS anger FROM alpha_rows';
+            $rows = $conn->query($sql);
+            if (!$rows) {
+                echo 'An SQL error occured.\n';
+                exit(1);
+            }
+
+            $response = array();
+            while ($row = $rows -> fetch(PDO::FETCH_ASSOC)) {
+                $latlon = split('&', str_replace(['lon=', 'lat='], '', $row['coords']));
+                $lt = $latlon[0];
+                $lg = $latlon[1];
+
+                $checkin = [
+                    'id' => intval($row['id']),
+                    'position' => [
+                        'lt' => $lt,
+                        'lg' => $lg
+                    ]
+                ];
+
+                array_push($response, $checkin);
+            }
             break;
-        case '/map/0':
-        case '/map/1':
-        case '/map/2':
-        case '/map/3':
-            header('Content-type: application/json');
-            header("HTTP/1.0 200 OK");
-            echo '[
-                      {
-                          "id": 1,
-                          "heat": 1
-                      },
-                      {
-                          "id": 2,
-                          "heat": 5
-                      }
-                  ]';
-            break;
-        default:
-            header("HTTP/1.0 400 Bad Request");
+    }
+
+    if ($response) {
+        header('Content-type: application/json');
+        header("HTTP/1.0 200 OK");
+        echo json_encode($response);
+        exit();
+    } else {
+        header("HTTP/1.0 400 Bad Request");
+        exit();
     }
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['SCRIPT_NAME'] == '/check-in') {
+    var_dump(file_get_contents('php://input'));
     header("HTTP/1.0 201 Created");
 }
 
