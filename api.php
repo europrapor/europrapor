da088edbcc85bc2eaa5d9c8f4d7aa5a5a73fe294
Oleksii Matiiasevych <lastperson@gmail.com>
2013-12-17 22:00:21 +0200

    Update API to support POST.

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $response = '';
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (preg_match('/^\/map\/(\w+)$/', $_SERVER['SCRIPT_NAME'], $matches) && in_array($matches[1], $maps)) {
         include('mysql_pdo_conn.php');
-        $sql = 'SELECT id, unixtime, rep1 AS coords, rep2 AS security, rep3 AS joy, rep4 AS fear, rep5 AS determination, divers AS anger FROM alpha_rows';
+        $sql = 'SELECT id, unixtime, rep1 AS coords, rep2 AS privacy, rep3 AS joy, rep4 AS fear, rep5 AS determination, divers AS anger FROM alpha_rows';
         $rows = $conn->query($sql);
         if (!$rows) {
             echo 'An SQL error occured.\n';
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             break;
         case '/map':
             include('mysql_pdo_conn.php');
-            $sql = 'SELECT id, unixtime, rep1 AS coords, rep2 AS security, rep3 AS joy, rep4 AS fear, rep5 AS determination, divers AS anger FROM alpha_rows';
+            $sql = 'SELECT id, unixtime, rep1 AS coords, rep2 AS privacy, rep3 AS joy, rep4 AS fear, rep5 AS determination, divers AS anger FROM alpha_rows';
             $rows = $conn->query($sql);
             if (!$rows) {
                 echo 'An SQL error occured.\n';
@@ -60,14 +60,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         header('Content-type: application/json');
         header("HTTP/1.0 200 OK");
         echo json_encode($response);
-        exit();
     } else {
         header("HTTP/1.0 400 Bad Request");
-        exit();
     }
-} elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['SCRIPT_NAME'] == '/check-in') {
-    var_dump(file_get_contents('php://input'));
-    header("HTTP/1.0 201 Created");
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['SCRIPT_NAME'] == '/check-in' && isset($_POST['check-in'])) {
+    $checkin = json_decode($_POST['check-in'], true);
+    $mental_state = $checkin['mental_state'];
+    include('mysql_pdo_conn.php');
+
+    $datetime = new DateTime('NOW', new DateTimeZone('UTC'));
+    $sql = 'INSERT INTO alpha_rows (time, unixtime, rep1, rep2, rep3, rep4, rep5, divers) '.
+        'VALUES ("'.date('Y-m-d h:i:s').'",'.$datetime->format('U').',"lat='.
+        strval($checkin['position']['lt']).'&lon='.strval($checkin['position']['lg']).'",'.strval($checkin['privacy']).','.
+        strval($mental_state['joy']).','.strval($mental_state['fear']).','.strval($mental_state['determination']).','.strval($mental_state['anger']).')';
+
+    if ($conn->exec($sql)) {
+        header("HTTP/1.0 201 Created");
+    } else {
+        header("HTTP/1.0 400 Bad Request");
+    }
 }
 
 ?>
