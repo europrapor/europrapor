a6ebf6ee0c1029a2c5a5f21babfbebb7d0dcd63c
roman01la <roman01la@romanliutikov.com>
2013-12-25 07:36:40 +0200

    add privacy slider & allow to perform check-in

--- ng-client/app/scripts/controllers/checkin-ctrl.js
+++ ng-client/app/scripts/controllers/checkin-ctrl.js
@@ -3,20 +3,47 @@
 'use strict';
 
 angular.module('ngEuroPraporApp')
-  .controller('CheckinCtrl', function ($scope, $timeout) {
+  .controller('CheckinCtrl', function ($scope, $timeout, Checkin) {
 
     $scope.sliders = {
-        anger:          0
-      , joy:            2
-      , determination:  7
+        anger:          5
+      , joy:            5
+      , determination:  5
       , fear:           5
+      , privacy:        10
+    };
+
+    $scope.performCheckin = function() {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var checkin;
+
+        $scope.position = {
+          lt: position.coords.latitude,
+          lg: position.coords.longitude
+        };
+
+        checkin = new Checkin({
+          position: $scope.position,
+          mental_state: _.omit($scope.sliders, 'privacy'),
+          privacy: $scope.sliders.privacy
+        });
+
+        checkin.$save();
+      });
     };
 
   })
   .directive('ngSlider', function() {
     var rangeSlider = function ($scope, $element, $attrs) {
+      if ($attrs.id == 'privacy')
+          $element[0].classList.add('privacy');
+
       new SliderControl($element[0], 0, 10, {
         label: $attrs.id.toUpperCase()
+      , initialValue: $scope.sliders[$attrs.id]
+      , onslide: function() {
+          $scope.sliders[$attrs.id] = this.getValue();
+        }
       });
     };
 
