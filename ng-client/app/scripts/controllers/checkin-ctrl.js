1f78dd526b60fead3a49f77468b0a206c06c2d5c
roman01la <roman01la@romanliutikov.com>
2013-12-25 04:55:41 +0200

    check-in view & range sliders

--- ng-client/app/scripts/controllers/checkin-ctrl.js
+++ ng-client/app/scripts/controllers/checkin-ctrl.js
@@ -1,30 +1,24 @@
+/* global SliderControl */
+
 'use strict';
 
 angular.module('ngEuroPraporApp')
   .controller('CheckinCtrl', function ($scope, $timeout) {
-    
+
     $scope.sliders = {
-        anger:   0
-      , joy:     2
-      , determ:  7
-      , fear:    5
+        anger:          0
+      , joy:            2
+      , determination:  7
+      , fear:           5
     };
 
-    $scope.my_func = function() {
-      console.log("here");
+  })
+  .directive('ngSlider', function() {
+    var rangeSlider = function ($scope, $element, $attrs) {
+      new SliderControl($element[0], 0, 10, {
+        label: $attrs.id.toUpperCase()
+      });
     };
-    
-    $scope.$watch('sliders', function(newValue, oldValue) {
-      console.log("watcher fired: ", newValue, oldValue);
-    });
 
-    $timeout(function() {
-      $scope.sliders = {
-          anger:   1
-        , joy:     2
-        , determ:  7
-        , fear:    5
-      };
-    }, 2000 );
-
+    return rangeSlider;
   });
---
+++ ng-client/app/scripts/controllers/header-ctrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ngEuroPraporApp')
+  .controller('HeaderCtrl', function ($scope) {
+
+  });
