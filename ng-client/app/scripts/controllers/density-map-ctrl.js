5f5c161c9957f95beea2828682a5ad81fbd1441e
roman01la <roman01la@romanliutikov.com>
2013-12-28 15:55:49 +0200

    cache map tiles to localStorage

--- ng-client/app/scripts/controllers/density-map-ctrl.js
+++ ng-client/app/scripts/controllers/density-map-ctrl.js
@@ -4,7 +4,10 @@
 
 angular.module('ngEuroPraporApp')
   .controller('DensityMapCtrl', function ($scope, $timeout, DensityMap) {
+    // fetch geodata
     var densityData = DensityMap.query(function() {
+      var cacheRead1, cacheRead2, cacheWrite;
+
       var OLReadyData = {
             max: 1,
             data: []
@@ -12,6 +15,7 @@ angular.module('ngEuroPraporApp')
           dataLength = densityData.length,
           densityMap, OSMLayer, densityLayer;
 
+      // convert geodata to Open Layers ready data
       while (dataLength--) {
         OLReadyData.data.push({
             lonlat: new OpenLayers.LonLat(
@@ -22,9 +26,13 @@ angular.module('ngEuroPraporApp')
         });
       }
 
+      // create a map
       densityMap = new OpenLayers.Map('density-map');
+
+      // create OSM layer
       OSMLayer = new OpenLayers.Layer.OSM();
 
+      // create density map layer
       densityLayer = new OpenLayers.Layer.Heatmap('DensityLayer', densityMap, OSMLayer,
         {
           visible: true,
@@ -37,9 +45,49 @@ angular.module('ngEuroPraporApp')
         }
       );
 
+      // try cache before loading from remote resource
+      cacheRead1 = new OpenLayers.Control.CacheRead({
+        eventListeners: {
+          activate: function() {
+            cacheRead2.deactivate();
+          }
+        }
+      });
+
+      // try loading from remote resource and fall back to cache
+      cacheRead2 = new OpenLayers.Control.CacheRead({
+        autoActivate: false,
+        fetchEvent: 'tileerror',
+        eventListeners: {
+          activate: function() {
+            cacheRead1.deactivate();
+          }
+        }
+      });
+
+      // cache map tiles to localStorage
+      cacheWrite = new OpenLayers.Control.CacheWrite({
+        imageFormat: 'image/jpeg',
+        eventListeners: {
+          cachefull: function() {}
+        }
+      });
+
+      // add layers to map
       densityMap.addLayers([OSMLayer, densityLayer]);
+
+      // set initial zoom level
       densityMap.zoomToMaxExtent();
 
+      // add cache controllers
+      densityMap.addControls([cacheRead1, cacheRead2, cacheWrite]);
+
+      // put Open Layers ready data to the map
       densityLayer.setDataSet(OLReadyData);
+
+      // init cache controllers
+      cacheRead1.activate();
+      cacheRead2.activate();
+      cacheWrite.activate();
     });
   });
