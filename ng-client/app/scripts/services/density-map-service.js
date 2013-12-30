b37459d5741b2454a7a3b62f9879360506d5d61d
roman01la <roman01la@romanliutikov.com>
2013-12-30 12:35:14 +0200

    return to devel API

--- ng-client/app/scripts/services/density-map-service.js
+++ ng-client/app/scripts/services/density-map-service.js
@@ -2,5 +2,5 @@
 
 angular.module('ngEuroPraporApp')
   .factory('DensityMap', function ($resource) {
-    return $resource('http://api.europrapor.oxogamestudio.com/map');
+    return $resource('http://devel.api.europrapor.oxogamestudio.com/map');
   });
