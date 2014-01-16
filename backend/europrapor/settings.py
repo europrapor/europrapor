1994ecfad2a643c2ecf30faf3ec5bc024ce15491
dottymind <dottymind@gmail.com>
2014-01-16 16:35:34 +0200

    Backend stub written in python.

---
+++ backend/europrapor/settings.py
@@ -0,0 +1,7 @@
+# Global settings
+DEBUG_MODE = True
+
+# Web application network settings
+HTTP_ADDR = '127.0.0.1'
+HTTP_PORT = '8000'
+HTTP_WORKERS = 0  # Set to 0 to automaticaly detect cores count.
