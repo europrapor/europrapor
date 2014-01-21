6a60a8abfb9aca548c75a3c7406da4da050d994a
dottymind <dottymind@gmail.com>
2014-01-21 16:27:34 +0200

    add checkin functionality

--- backend/europrapor/checkin.py
+++ backend/europrapor/checkin.py
@@ -1,8 +1,22 @@
-import logging
+from db import Database
+from tornado.escape import json_decode
 from tornado.web import RequestHandler
 
 
 class CheckinHandler(RequestHandler):
 
     def post(self):
-        logging.debug("Accepted!")
+        checkin = json_decode(self.request.body)
+        position = checkin['position']
+        mental_state = checkin['mental_state']
+        query = ('INSERT INTO beta_rows (lt, lg, privacy, joy, fear, determination, anger)'
+                 ' VALUES ({lt}, {lg}, {privacy}, {joy}, {fear}, {determination}, {anger})'
+                 .format(lt=str(position['lt']),
+                         lg=str(position['lg']),
+                         privacy=str(checkin['privacy']),
+                         joy=str(mental_state['joy']),
+                         fear=str(mental_state['fear']),
+                         determination=str(mental_state['determination']),
+                         anger=str(mental_state['anger'])))
+        print(query)
+        Database.get_connection().execute(query)
--- backend/tests/test_checkin.py
+++ backend/tests/test_checkin.py
@@ -6,7 +6,20 @@ import unittest
 class TestCheckin(unittest.TestCase):
 
     def test_checkin(self):
-        response = post('http://localhost:8000/check-in', data=json_encode({'key': 'value'}))
+        data = {
+            'position': {
+                'lt': 35.45645,
+                'lg': 67.98798
+            },
+            'mental_state': {
+                'joy': 2,
+                'determination': 3,
+                'anger': 1,
+                'fear': 0
+            },
+            'privacy': 10
+        }
+        response = post('http://localhost:8000/check-in', data=json_encode(data))
         self.assertEqual(response.status_code, 200)
 
 
