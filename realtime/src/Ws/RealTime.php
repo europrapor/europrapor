15974664a518c597b1ad84c0198c61c7f4e230e9
Oleksii Matiiasevych <lastperson@gmail.com>
2014-01-22 14:59:16 +0200

    Remove unnecessary logging.

--- realtime/src/Ws/RealTime.php
+++ realtime/src/Ws/RealTime.php
@@ -13,8 +13,6 @@ class RealTime implements MessageComponentInterface {
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
-
-        echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
@@ -23,8 +21,6 @@ class RealTime implements MessageComponentInterface {
     public function onClose(ConnectionInterface $conn) {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
-
-        echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
     public function onCheckIn($checkin) {
